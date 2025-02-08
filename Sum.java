b2130dc386c8af7831791d01020393e3404e785b
sapna patil <swapnalipatil2801@gmail.com>
2025-02-08 23:11:20 +0530

    comiit all

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String[] args) {
         int a=10;
         int b=10;
-        System.out.println(a+b);
+        System.out.println(a+b+" ");
     }
 }
